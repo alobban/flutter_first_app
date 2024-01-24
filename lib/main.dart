01533e6db07585f2656a0da2d394c6ed70cac8f8
Andrew Lobban <alobban@Andrews-MacBook-Pro.local>
2024-01-24 15:01:34 -0600

    Add backgroundColor config

--- lib/main.dart
+++ lib/main.dart
@@ -4,6 +4,7 @@ void main() {
   runApp(
     const MaterialApp(
       home: Scaffold(
+        backgroundColor: Color.fromARGB(255, 47, 7, 117),
         body: Center(
           child: Text('Hello World!'),
         ),
