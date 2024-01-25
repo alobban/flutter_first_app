64762400d64fd9a667e3dc1821a9f8fea8682f42
Andrew Lobban <alobban@Andrews-MacBook-Pro.local>
2024-01-24 20:54:50 -0600

    Extract custom widget into its own file

---
+++ lib/gradient_container.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+
+class GradientContainer extends StatelessWidget {
+  const GradientContainer({super.key}); // constructor Function
+
+  @override
+  Widget build(context) {
+    return Container(
+      decoration: const BoxDecoration(
+        gradient: LinearGradient(
+          colors: [ 
+            Color.fromARGB(255, 25, 1, 67),
+            Color.fromARGB(255, 45, 7, 98),
+          ],
+          begin: Alignment.topLeft,
+          end: Alignment.bottomRight,
+        ),
+      ),
+      child: const Center(
+        child: Text('Hello World!', style: TextStyle(
+          color: Colors.white,
+          fontSize: 28,
+        ),),
+      ),
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+import 'package:first_app/gradient_container.dart';
+
 void main() {
   runApp(
     const MaterialApp(
@@ -8,30 +10,4 @@ void main() {
       ),
     ),
   );
-}
-
-class GradientContainer extends StatelessWidget {
-  const GradientContainer({super.key}); // constructor Function
-
-  @override
-  Widget build(context) {
-    return Container(
-      decoration: const BoxDecoration(
-        gradient: LinearGradient(
-          colors: [ 
-            Color.fromARGB(255, 25, 1, 67),
-            Color.fromARGB(255, 45, 7, 98),
-          ],
-          begin: Alignment.topLeft,
-          end: Alignment.bottomRight,
-        ),
-      ),
-      child: const Center(
-        child: Text('Hello World!', style: TextStyle(
-          color: Colors.white,
-          fontSize: 28,
-        ),),
-      ),
-    );
-  }
 }
