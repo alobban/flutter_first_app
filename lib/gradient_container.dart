a49002c5e1ddaa169651c026c289dccf18ea0290
Andrew Lobban <andrewglobban@gmail.com>
2024-01-24 23:46:29 -0600

    Add button functionality with StatefulWidget

--- lib/gradient_container.dart
+++ lib/gradient_container.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+import 'package:first_app/dice_roller.dart';
+
 const startAlignment = Alignment.topLeft;
 const endAlignment = Alignment.bottomRight;
 
@@ -8,8 +10,6 @@ class GradientContainer extends StatelessWidget {
       {super.key, required this.colors}); // constructor Function
 
   final List<Color> colors;
-
-  void rollDice() {}
 
   @override
   Widget build(context) {
@@ -21,27 +21,8 @@ class GradientContainer extends StatelessWidget {
           end: endAlignment,
         ),
       ),
-      child: Center(
-        child: Column(mainAxisSize: MainAxisSize.min, children: [
-          Image.asset(
-            'assets/images/dice-2.png',
-            width: 200,
-          ),
-          const SizedBox(height: 20), // addings 20px box between image and button
-          TextButton(
-            onPressed: rollDice,
-            style: TextButton.styleFrom(
-              // padding: const EdgeInsets.only(
-              //   top: 20,
-              // ),
-              foregroundColor: Colors.white,
-              textStyle: const TextStyle(
-                fontSize: 28,
-              ),
-            ),
-            child: const Text('Roll Dice'),
-          ),
-        ]),
+      child: const Center(
+        child: DiceRoller(),
       ),
     );
   }
