5ee6cc52721c0b626a47556a69d2f4a422d0ad84
Andrew Lobban <alobban@Andrews-MacBook-Pro.local>
2024-01-24 20:44:57 -0600

    Refactor into Custom Widget

--- lib/main.dart
+++ lib/main.dart
@@ -4,25 +4,32 @@ void main() {
   runApp(
     MaterialApp(
       home: Scaffold(
-        body: Container(
-          decoration: const BoxDecoration(
-            gradient: LinearGradient(
-              colors: [ 
-                Color.fromARGB(255, 25, 1, 67),
-              Color.fromARGB(255, 45, 7, 98),
-              ],
-              begin: Alignment.topLeft,
-              end: Alignment.bottomRight,
-            ),
-          ),
-          child: const Center(
-            child: Text('Hello World!', style: TextStyle(
-              color: Colors.white,
-              fontSize: 28,
-            ),),
-          ),
-        ),
+        body: GradientContainer(),
       ),
     ),
   );
+}
+
+class GradientContainer extends StatelessWidget {
+  @override
+  Widget build(context) {
+    return Container(
+      decoration: const BoxDecoration(
+        gradient: LinearGradient(
+          colors: [ 
+            Color.fromARGB(255, 25, 1, 67),
+          Color.fromARGB(255, 45, 7, 98),
+          ],
+          begin: Alignment.topLeft,
+          end: Alignment.bottomRight,
+        ),
+      ),
+      child: const Center(
+        child: Text('Hello World!', style: TextStyle(
+          color: Colors.white,
+          fontSize: 28,
+        ),),
+      ),
+    );
+  }
 }
